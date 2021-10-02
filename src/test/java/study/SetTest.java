58cdf46df91e54e14fe659be31ac832ab5aba504
apdkeb <apdkeb@naver.com>
2021-10-02 14:14:58 +0900

    test : Set 성공 & 실패 테스트 추가
    2021. 10. 02.
    - Set Collection 성공 & 실패 동시 확인하는 테스트 케이스 추가
    
    Resolver : loop-study
    See Also : NextStep

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
@@ -31,7 +32,13 @@ public class SetTest {
 
     @ParameterizedTest
     @ValueSource(ints = {1,2,3})
-    void Set_값_확인(int number) {
+    void Set_값_확인_1(int number) {
         assertThat(numbers).contains(number);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"0:false","1:true","2:true","3:true","4:false"}, delimiter = ':')
+    void Set_값_확인_2(int inputNumber, boolean isFree) throws Exception {
+        assertThat(numbers.contains(inputNumber)).isEqualTo(isFree);
     }
 }
