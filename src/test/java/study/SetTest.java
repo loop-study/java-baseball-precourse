e68e84487286c97efedc5bd7af106849cbaf217a
apdkeb <apdkeb@naver.com>
2021-10-02 14:08:12 +0900

    test : Set 값 확인 테스트 추가
    2021. 10. 02.
    - Set Collection 값 존재여부 테스트 추가
    
    Resolver : loop-study
    See Also : NextStep

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -3,6 +3,8 @@ package study;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -27,5 +29,9 @@ public class SetTest {
         assertThat(numbers).hasSize(3);
     }
 
-
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,3})
+    void Set_값_확인(int number) {
+        assertThat(numbers).contains(number);
+    }
 }
