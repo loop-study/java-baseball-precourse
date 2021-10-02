e0c07a4cb370c09c178da1d687edb6dc778d46d2
apdkeb <apdkeb@naver.com>
2021-10-02 14:06:22 +0900

    test : Set 사이즈 테스트 추가
    2021. 10. 02.
    - Set Collection 사이즈 확인 테스트 추가
    
    Resolver : loop-study
    See Also : NextStep

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -1,10 +1,15 @@
 package study;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 import java.util.HashSet;
 import java.util.Set;
 
+import static org.assertj.core.api.Assertions.*;
+
+@DisplayName("Set 학습테스트")
 public class SetTest {
     private Set<Integer> numbers;
 
@@ -15,6 +20,11 @@ public class SetTest {
         numbers.add(1);
         numbers.add(2);
         numbers.add(3);
+    }
+
+    @Test
+    void Set_사이즈_확인() {
+        assertThat(numbers).hasSize(3);
     }
 
 
