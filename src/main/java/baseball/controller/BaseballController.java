076c24fd264de431bc20712cc4020558ce5fb543
apdkeb <apdkeb@naver.com>
2021-10-03 17:52:35 +0900

    feat : 숫자야구 컨트롤러 생성
    2021. 10. 03.
    - 컨트롤러 추가
    
    Resolver : loop-study
    See Also : NextStep

---
+++ src/main/java/baseball/controller/BaseballController.java
@@ -0,0 +1,13 @@
+package baseball.controller;
+
+public class BaseballController {
+    private InputView inputView;
+
+    public BaseballController() {
+        inputView = new InputView();
+    }
+
+    public void run() {
+
+    }
+}
