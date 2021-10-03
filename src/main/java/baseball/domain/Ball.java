599fc61170aab3c60a4d98ec86bd59877b708f6a
apdkeb <apdkeb@naver.com>
2021-10-03 13:46:07 +0900

    feat : 야구공 생성 예외로직 추가
    2021. 10. 03.
    - 숫자범위 1~9 아니면 예외로직 추가
    
    Resolver : loop-study
    See Also : NextStep

--- src/main/java/baseball/domain/Ball.java
+++ src/main/java/baseball/domain/Ball.java
@@ -1,9 +1,22 @@
 package baseball.domain;
 
+
 public class Ball {
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 9;
+    private static final String ERROR_NUMBER_LANGE = "야구공은 1 ~ 9 의 숫자를 가져야합니다.";
+
     private final int number;
 
     public Ball(int number) {
+        validationNumberLange(number);
         this.number = number;
+    }
+
+    private void validationNumberLange(int number) {
+        if (number < MIN_NUMBER
+            || number > MAX_NUMBER) {
+            throw new IllegalArgumentException(ERROR_NUMBER_LANGE);
+        }
     }
 }
