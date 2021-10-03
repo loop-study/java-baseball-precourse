9a9e5f785e4b7c7a5060956bee8545debc2f376d
apdkeb <apdkeb@naver.com>
2021-10-03 17:25:17 +0900

    feat : Score 볼 로직 추가
    2021. 10. 03.
    - 볼 계산 로직 추가
    
    Resolver : loop-study
    See Also : NextStep

--- src/main/java/baseball/domain/Score.java
+++ src/main/java/baseball/domain/Score.java
@@ -23,8 +23,9 @@ public class Score {
 
     public static Score of(List<Ball> playerBalls, List<Ball> randomBalls) {
         int strikeCount = strikeZone(playerBalls, randomBalls);
+        int ballCount = ballZone(playerBalls, randomBalls);
 
-        return new Score(strikeCount, 0);
+        return new Score(strikeCount, ballCount);
     }
 
     private static int strikeZone(List<Ball> playerBalls, List<Ball> randomBalls) {
@@ -37,6 +38,22 @@ public class Score {
 
     private static int isStrike(Ball playerBall, Ball randomBall) {
         if (playerBall.equals(randomBall)) {
+            return ONE;
+        };
+        return ZERO;
+    }
+
+    private static int ballZone(List<Ball> playerBalls, List<Ball> randomBalls) {
+        int count = 0;
+        for (int i = 0; i < playerBalls.size(); i++) {
+            count += isBall(playerBalls.get(i), randomBalls.get(i), randomBalls);
+        }
+        return count;
+    }
+
+    private static int isBall(Ball playerBall, Ball randomBall, List<Ball> randomBalls) {
+        if (!playerBall.equals(randomBall)
+            && randomBalls.contains(playerBall)) {
             return ONE;
         };
         return ZERO;
