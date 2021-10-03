1284d5b0234c2ca9dfe9d8491b769c93ba1fe3e7
apdkeb <apdkeb@naver.com>
2021-10-03 15:13:53 +0900

    feat : balls 중복확인 로직 추가
    2021. 10. 03.
    - 중복확인 로직 추가
    
    Resolver : loop-study
    See Also : NextStep

--- src/main/java/baseball/domain/Balls.java
+++ src/main/java/baseball/domain/Balls.java
@@ -1,12 +1,19 @@
 package baseball.domain;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Balls {
+    private static final int BALL_SIZE = 3;
+    private static final String ERROR_OVERLAP_BALL = "중복되는 숫자가 있습니다.";
+
+
     private final List<Ball> balls;
 
     public Balls(List<Ball> balls) {
+        validationOverlapBall(balls);
         this.balls = balls;
     }
 
@@ -16,5 +23,12 @@ public class Balls {
         randomBalls.add(Ball.of());
         randomBalls.add(Ball.of());
         return new Balls(randomBalls);
+    }
+
+    private void validationOverlapBall(List<Ball> balls) {
+        Set<Ball> ballSet = new HashSet<>(balls);
+        if (ballSet.size() != BALL_SIZE) {
+            throw new IllegalArgumentException(ERROR_OVERLAP_BALL);
+        }
     }
 }
