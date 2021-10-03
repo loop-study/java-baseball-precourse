b193cc24bffb5216f84bf299a728b195105a8f2e
apdkeb <apdkeb@naver.com>
2021-10-03 15:06:58 +0900

    test : balls 중복 테스트 추가
    2021. 10. 03.
    - 중복된 공이면 예외확인 테스트 추가
    
    Resolver : loop-study
    See Also : NextStep

--- src/test/java/baseball/domain/BallsTest.java
+++ src/test/java/baseball/domain/BallsTest.java
@@ -2,6 +2,9 @@ package baseball.domain;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.*;
 
 public class BallsTest {
@@ -11,5 +14,21 @@ public class BallsTest {
         Balls balls = Balls.of();
 
         assertThat(balls).isNotNull();
+    }
+
+    @Test
+    void 중복_예외() {
+        int[] numbers = new int[]{1,1,3};
+
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new Balls(createBalls(numbers)));
+    }
+
+    private List<Ball> createBalls(int[] numbers) {
+        List<Ball> balls = new ArrayList<>();
+        for (int i = 0; i < numbers.length; i++) {
+            balls.add(new Ball(numbers[i]));
+        }
+        return balls;
     }
 }
